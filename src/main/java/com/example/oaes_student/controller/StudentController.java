a480e0bb4dbdfb0bff806ba0a97375c356edd3a0
Khushal Abrol <khushalabrol14@gmail.com>
2022-11-28 00:00:31 +0530

    Bug Fix: Resolved CORS error

--- src/main/java/com/example/oaes_student/controller/StudentController.java
+++ src/main/java/com/example/oaes_student/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/student")
 public class StudentController {
